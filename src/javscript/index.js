e9dc0859141f873827db79b6c11dbe11a6e998c2
Brayden Purtschert <bpurtschert@kevinmurphy.com.au>
2024-04-18 12:40:24 -0600

    Updated template to include logo and fonts

---
+++ src/javscript/index.js
@@ -0,0 +1,4 @@
+// Toggle the nav menu when the hamburger menu is clicked
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('open');
+});
